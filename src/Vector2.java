48ac88b80d7b3e60293365cdacf6b820fe23e3f1
Mgmaddox05 <125920278+Mgmaddox05@users.noreply.github.com>
2023-10-05 14:35:09 -0500

    Update 3

--- src/Vector2.java
+++ src/Vector2.java
@@ -1,15 +1,22 @@
 import java.util.*;
 
-public class Vector2 {
-    private double x;
-    private double y;
+public class Vector2 extends MathObject {
+    private int x;
+    private int y;
 
-    public Vector2(double x, double y) {
+    public Vector2(int x, int y) {
+        super(); 
         this.x = x;
         this.y = y;
     }
 
-    public double magnitude() {
+    public Vector2(int x, int y, String description) {
+        super(description); 
+        this.x = x;
+        this.y = y;
+    }
+
+    public int magnitude() {
         return Math.sqrt(x * x + y * y);
     }
 
@@ -25,4 +32,12 @@ public class Vector2 {
     public String toString() {
         return "Vector2(" + this.x + ", " + this.y + ")";
     }
+
+    @Override
+    public int getDimension() {
+        return 2; 
+    }
+
+    
 }
+
