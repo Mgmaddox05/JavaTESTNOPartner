b205234f08efde0ded9d15ff2635dd2dc79be9f3
Mgmaddox05 <125920278+Mgmaddox05@users.noreply.github.com>
2023-10-05 14:38:46 -0500

    Update 4

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Vector2 v1 = new Vector2(3, 4);
         Vector2 v2 = new Vector2(1, 2, "A 2D vector");
 
-        System.out.println(vector1.getDescription()); 
-        System.out.println(vector2.getDescription()); 
-        System.out.println("Vector2 1 Dimension: " + v1.getDimension()); 
-        System.out.println("Vector2 2 Dimension: " + vector2.getDimension());
+        System.out.println(v1.getDescription());  // Use 'v1' instead of 'vector1'
+        System.out.println(v2.getDescription());  // Use 'v2' instead of 'vector2'
+        System.out.println("Vector2 1 Dimension: " + v1.getDimension());
+        System.out.println("Vector2 2 Dimension: " + v2.getDimension());
     }
 }
--- src/MathObject.java
+++ src/MathObject.java
@@ -1,25 +1,23 @@
-public abstract class MathObject {
-        private String description;
+abstract class MathObject {
+    private String description;
 
-        // Default constructor
-        public MathObject() {
-            this.description = "No description available";
-        }
-    
-        // Constructor with a description parameter
-        public MathObject(String description) {
-            this.description = description;
-        }
-    
-        public String getDescription() {
-            return description;
-        }
-    
-        public void setDescription(String description) {
-            this.description = description;
-        }
-    
-        
-        public abstract int getDimension();
-    
+    // Default constructor
+    public MathObject() {
+        this.description = "No description available";
+    }
+
+    // Constructor with a description parameter
+    public MathObject(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract int getDimension();
 }
